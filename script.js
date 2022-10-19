5c240fa383ffe633ceae6c2dfcddf827335998a3
Lumi <shaicloud@gmail.com>
2022-10-19 02:04:35 -0400

    clear grid functionality added

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 //GRID SIZE
-const container = document.querySelector(".squareContainer");
+const squareContainer = document.querySelector(".squareContainer");
 const sizeElement = document.querySelector(".size");
 let size = sizeElement.value;
 
-//GRID COLOR
+//GRID COLOR STUFF
 const color = document.querySelector(".Color");
+const clearGrid = document.querySelector(".clearGrid");
 
 //ACTIONS
 let draw = false;
 
+//POPULATE THE GRID
 function fillcontainer(size) {
-	container.style.setProperty("--size", size);
+	squareContainer.style.setProperty("--size", size);
 	for (let i = 0; i < size * size; i++) {
 		const square = document.createElement("div");
 		square.classList.add("square");
 
-		square.addEventListener("mouseover", function () {
+		//SO YOU CAN DRAG THE MOUSE OVER TO DRAW
+		square.addEventListener("mouseover", () => {
 			if (!draw) {
 				return;
 			}
 			square.style.backgroundColor = color.value;
 		});
-		square.addEventListener("mousedown", function () {
+		//SO YOU CAN CLICK TO COLOR AND TO FILL THE CLICKED BOX WITH COLOR WHEN YOU CLICK > DRAG
+		square.addEventListener("mousedown", () => {
 			square.style.backgroundColor = color.value;
 		});
-		container.appendChild(square);
+		squareContainer.appendChild(square);
 	}
 }
 
+//DRAWING EVENT LISTENERS
 window.addEventListener("mousedown", () => (draw = true));
 window.addEventListener("mouse", () => (draw = false));
-
 window.addEventListener("mouseup", () => (draw = false));
+
+//CLEAR GRID BUTTON
+clearGrid.addEventListener("click", () => {
+	squareContainer.innerHTML = "";
+	fillcontainer(size);
+});
 
 fillcontainer(size);
